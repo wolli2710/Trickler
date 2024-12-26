d14b4cea9c39aa393a31478c180d77ce86956643
Wolfgang Vogl <wolfgang.vogl01@gmail.com>
2024-12-26 01:02:42 +0100

    first working Stepper version

--- app.js
+++ app.js
@@ -8,7 +8,60 @@ const port = 3000
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-var stepper1 = new Stepper(43.0, 0.2);
+var ctx = this;
+
+var tricklerStarted = false;
+var targetWeight = 43.0;
+var currentWeight = 0;
+
+var thresholdTarget = 0.2;
+var thresholdSlow = 2;
+var thresholdFast = 10;
+
+//Fast Stepper with 8 = stepPin and 10 = directionPin
+var stepper1 = new Stepper(8, 10, 10);
+//Slow Stepper with 12 = stepPin and 16 = directionPin
+var stepper2 = new Stepper(12, 16, 1000);
+
+var handleStepper = function(data){
+  if(tricklerStarted === true) {
+    currentWeight = parseFloat(data);
+    console.log('targetWeight :' + data + ' thresholdFast :' + currentWeight);
+
+    if(currentWeight < (targetWeight - thresholdFast)) {
+      console.log('targetWeight :' + targetWeight + ' thresholdFast :' + thresholdFast);
+      stepper1.move(8);
+
+ /*   } else if(this.currentWeight < (this.targetWeight - this.thresholdSlow)) {
+      this.stepper2.move(3);
+    } else if(this.currentWeight < (this.targetWeight - this.thresholdTarget)) {
+      this.stepper2.move(1);
+      */
+    } else {
+      tricklerStarted = false;
+    }
+  }
+}
+
+var updateTargetWeight = function(weight) {
+  if(typeof(weight) !== 'number') {
+    weight = parseFloat(weight);
+  }
+  ctx.targetWeight = weight;
+}
+
+var updateCurrentWeight = function(weight) {
+  if(typeof(weight) !== 'number') {
+    weight = parseFloat(weight);
+  }
+  ctx.currentWeight = weight;
+}
+
+var getScaleDataGnG = function(){
+  var esc = Buffer.alloc(1, 27);
+  var pn = Buffer.from("pn");
+  return Buffer.concat([esc, pn]);
+}
 
 const serialPort = new SerialPort({ 
 	path: '/dev/ttyUSB0',
@@ -18,22 +71,7 @@ const serialPort = new SerialPort({
   parity: 'none',
 	timeout: 0.1 });
 
-var getScaleDataGnG = function(){
-  var esc = Buffer.alloc(1, 27);
-  var pn = Buffer.from("pn");
-  return Buffer.concat([esc, pn]);
-}
-
 serialPort.on('open', () => setInterval(() => serialPort.write(getScaleDataGnG()), 10));
-
-serialPort.on('data', function (data) {
-  const stringData = data.toString();
-
-  if (stringData) {
-    stepper1.updateCurrentWeight(stringData);
-    //console.log(`data received: ${stringData}`);
-  }
-});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -41,36 +79,38 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
 
   serialPort.on('data', function (data) {
     const stringData = data.toString();
-    if (stringData) {
+    if (stringData && stepper1 !== null) {
+      handleStepper(stringData);
       io.emit('updateCurrentWeight', stringData);
-      stepper1.updateCurrentWeight(stringData);
     }
   });
 
   socket.on('updateTargetWeight', (msg) => {
-    if(stepper1 !== null){
-      stepper1.updateTargetWeight(msg);
-    }
+    ctx.updateTargetWeight(msg);
   });
 
   socket.on('startTrickler', () => {
     console.log('startTrickler ........');
+    tricklerStarted = true;
     stepper1.startTrickler();
-    //io.emit('updateCurrentWeight', msg);
   });
 
   socket.on('stopTrickler', () => {
-    console.log('stopTrickler ........'); 
-    stepper1 = new Stepper(43.0, 0.2);
+    console.log('stopTrickler **********'); 
+    //this.stepper1 = new Stepper(43.0, 0.2);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
 });
 
+// ############################################################
+// ######################## WEBSERVER #########################
+// ############################################################
 server.listen(port, () => {
   console.log(`Trickler app listening on port ${port}`);
 });
--- stepper.js
+++ stepper.js
@@ -2,101 +2,33 @@
 
 module.exports = class Stepper {
   gpio = require('rpio');
-  //Stepper Motor FAST
-  STEP_FAST_PIN = 8;
-  DIRECTION_FAST_PIN = 10;
-  STEP_SLOW_PIN = 12;
-  DIRECTION_SLOW_PIN = 16;
 
-  SPEED_FAST = 10;
-  SPEED_SLOW = 1000;
-  SPEED_SLOW2 = 1000;
-
-  currentWeight = 0;
-  thresholdTarget = 0.2;
-  targetWeight = 43.0;
+  stepPin = null;
+  directionPin = null;
+  speed = 0;
 
   THRESHOLD_FAST = 10;
   THRESHOLD_SLOW = 5;
   THRESHOLD_SLOW2 = 1;
 
-  constructor(targetWeight, thresholdTarget) {
-    this.targetWeight = targetWeight;
-    this.thresholdTarget = thresholdTarget;
-    this.gpio.open(this.STEP_FAST_PIN, this.gpio.OUTPUT, this.gpio.LOW);
-    this.gpio.open(this.DIRECTION_FAST_PIN, this.gpio.OUTPUT, this.gpio.LOW);
-
-    //Stepper Motor SLOW
-    this.gpio.open(this.STEP_SLOW_PIN, this.gpio.OUTPUT, this.gpio.LOW);
-    this.gpio.open(this.DIRECTION_SLOW_PIN, this.gpio.OUTPUT, this.gpio.LOW);
+  constructor(_stepPin, _directionPin, _speed) {
+    this.speed = _speed;
+    this.stepPin = _stepPin;
+    this.directionPin = _directionPin;
+    this.gpio.open(this.stepPin, this.gpio.OUTPUT, this.gpio.LOW);
+    this.gpio.open(this.directionPin, this.gpio.OUTPUT, this.gpio.LOW);
   };
 
-  moveSlow2() {
-    this.gpio.write(this.STEP_SLOW_PIN, this.gpio.HIGH);
-    this.gpio.msleep(this.SPEED_SLOW2);
-    this.gpio.write(this.STEP_SLOW_PIN, gpio.LOW);
-    this.gpio.msleep(this.SPEED_SLOW2);
-    console.log("SLOW_2: "); 
-    if(this.currentWeight <= (this.targetWeight)) {
-      this.moveSlow2();
-    } else {
-      console.log("Target Weight Reached");
+  move(steps) {
+    for (let i = 0; i < steps; i++) {
+      this.gpio.write(this.stepPin, this.gpio.HIGH);
+      this.gpio.msleep(this.speed);
+      this.gpio.write(this.stepPin, this.gpio.LOW);
+      this.gpio.msleep(this.speed);
     }
   };
   
-  moveSlow() {
-    this.gpio.write(this.STEP_SLOW_PIN, this.gpio.HIGH);
-    this.gpio.msleep(this.SPEED_SLOW);
-    this.gpio.write(this.STEP_SLOW_PIN, this.gpio.LOW);
-    this.gpio.msleep(this.SPEED_SLOW);
-    console.log("SLOW: ");  
-    if(this.currentWeight < (this.targetWeight - this.THRESHOLD_SLOW)) {
-      this.moveSlow();
-    } else {
-      this.moveSlow2();
-    }
-  };
-  
-  moveFast() {
-    this.gpio.write(this.STEP_FAST_PIN, this.gpio.HIGH);
-    this.gpio.msleep(this.SPEED_FAST);
-    this.gpio.write(this.STEP_FAST_PIN, this.gpio.LOW);
-    this.gpio.msleep(this.SPEED_FAST);
-    console.log("current: " + this.currentWeight + " target: " + this.targetWeight);
-    console.log("diff: " + (this.targetWeight - this.THRESHOLD_FAST));
-
-    if(this.currentWeight < (this.targetWeight - this.THRESHOLD_FAST)) {
-      this.moveFast();
-    } else {
-      this.moveSlow();
-    }
-  };
-
   startTrickler() {
-    this.gpio.write(this.DIRECTION_FAST_PIN, this.gpio.HIGH);
-  
-    if(this.currentWeight == 0 ) {
-      console.log("StartTrickler Current Weight: " + this.currentWeight);
-      this.moveFast();
-    } else {
-      console.log("not 0");
-      this.gpio.sleep(1);
-      this.startTrickler();
-    }
+    this.gpio.write(this.directionPin, this.gpio.HIGH);
   };
-
-  updateTargetWeight(weight) {
-    if(typeof(weight) !== 'number') {
-      weight = parseFloat(weight);
-      //console.log(weight);
-    }
-    this.targetWeight = weight;
-  }
-
-  updateCurrentWeight(weight) {
-    if(typeof(weight) !== 'number') {
-      weight = parseFloat(weight);
-    }
-    this.currentWeight = weight;
-  }
 }
